f9426ee654080f54f985c322c63edfd3a5c6e9ac
Pedro Cruz <pscruzzz@outlook.com>
2024-01-01 23:38:35 -0300

    feat: new session

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,5 +1,5 @@
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
-import { marshall } from '@aws-sdk/util-dynamodb';
+import { DynamoDBClient, PutItemCommand, GetItemCommand } from '@aws-sdk/client-dynamodb';
+import { marshall,unmarshall } from '@aws-sdk/util-dynamodb';
 
 interface ISession {
   id: string;
@@ -20,6 +20,33 @@ export async function POST(request: Request) {
 
   if (response.user === undefined) {
     return new Response('Bad Request', { status: 400 });
+  }
+
+  const getItemCommand = new GetItemCommand({
+    TableName: "resistance",
+    Key: marshall({ id: response.sessionId })
+  });
+
+  const client = new DynamoDBClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? ""
+    }
+  });
+
+  try {
+    const { Item } = await client.send(getItemCommand);
+
+    if (Item) {
+      const existingSession = unmarshall(Item) as ISession;
+      if (existingSession.roles[response.user]) {
+        return new Response('User already registered', { status: 200 });
+      }
+    }
+  } catch (e) {
+    console.error("Error getting item into DynamoDB", e);
+    return new Response('Internal Server Error', { status: 500 });
   }
 
   const newSession: ISession = {
@@ -43,14 +70,6 @@ export async function POST(request: Request) {
       5: []
     },
   };
-
-  const client = new DynamoDBClient({
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? ""
-    }
-  });
 
   const putCommand = new PutItemCommand({
     TableName: "resistance",
