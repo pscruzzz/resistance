c083752bb67afc2454a7d4d0db1f7164b4460a19
Pedro Cruz <pscruzzz@outlook.com>
2024-01-03 20:15:45 -0300

    fix: improve jitter

--- src/app/api/_utils/generateJitter.ts
+++ src/app/api/_utils/generateJitter.ts
@@ -3,7 +3,7 @@ export async function generateJitter() {
   const now = new Date().getTime();
 
   // Use the last digit of the timestamp to determine the jitter (1 to 3 seconds)
-  const jitter = (now % 10) * 200 + 1000; // Will result in a number between 1000 and 3000
+  const jitter = (now % 10) * 1000; // Will result in a number between 1000 and 3000
 
   // Return a new Promise that resolves after the jitter time
   return new Promise<void>(resolve => {
